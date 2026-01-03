79f46fd569bccd3bf327af581dbdb3e05d14b68b
Ritik Negi <negiritik2022@gmai.com>
2026-01-03 18:42:56 +0530

    changes in mini.pair plugin

--- lua/plugins/mini-pairs.lua
+++ lua/plugins/mini-pairs.lua
@@ -2,5 +2,10 @@ pack("nvim-mini/mini.pairs")
 
 local mini_pairs = require("mini.pairs")
 
-mini_pairs.setup({})
-
+mini_pairs.setup({
+	mappings = {
+		["<"] = { action = "open", pair = "<>", neigh_pattern = "[^\\]." },
+		[">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+		["|"] = { action = "open", pair = "||", neigh_pattern = "[^\\]." },
+	},
+})
